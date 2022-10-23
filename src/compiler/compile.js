526a3d7dba893e1d303aefbe632344ee37ab9e02
awspi <206002328@qq.com>
2022-10-23 18:02:42 +0800

    init project

--- src/compiler/compile.js
+++
@@ -1,6 +0,0 @@
-import { parse } from './parse'
-import { generate } from './codegen'
-export const compile = (template) => {
-  const ast = parse(template)
-  return generate(ast)
-}
--- src/runtime/index.js
+++
@@ -1,6 +0,0 @@
-export { h, Text, Fragment } from './vnode';
-export { render } from './render';
-export { createApp, resolveComponent } from './createApp';
-export { queueJob, nextTick } from './scheduler';
-export { renderList } from './helpers/renderList';
-// export { withModel } from './helpers/vModel';
